9bdc8d546885c39815c9581a66d38bceda0a42c0
comxiaomingpeng <269539377@qq.com>
2018-01-03 22:12:40 +0800

    测试一下

---
+++ test/src/test/nnnn.java
@@ -0,0 +1,12 @@
+package test;
+
+public class nnnn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("jjjrs");
+		System.exit(0);
+		
+	}
+
+}
